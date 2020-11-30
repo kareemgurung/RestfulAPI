c82f47759f70a5b095a5bd3ca1e08280484b0f26
Kareem Gurung <kareemgurung@Kareems-MacBook-Pro.local>
2020-11-30 15:15:42 +0000

    connected to database

--- server/index.js
+++ server/index.js
@@ -1,25 +1,39 @@
+require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
 //Routes
+app.get('/', (req, res) => {
+    res.send("I am going to live.");
+    console.log('No you are not going to live');
+    
+})
+
 
 //Create 
-app.get('/posts', (req, res) ={
-    try {
-        const post = req.body;
+// app.get('/posts', (req, res) ={
+//     try {
+//         const post = req.body;
 
-        res.json(post);
-    } catch (error) {
-        console.error(error.message);
-    }
-})
+//         res.json(post);
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// })
 //Read
 
 //Update
 
 //Delete
 
+//Connect to db
+
+mongoose.connect(
+    process.env.DB_URL,
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    () => console.log("Server is connected to database."));
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
+
