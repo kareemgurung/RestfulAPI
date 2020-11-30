c38653d0fd445af1d01f5f3b2adb968ee1249b74
Kareem Gurung <kareemgurung@Kareems-MacBook-Pro.local>
2020-11-30 18:09:16 +0000

    created crud operation for posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,17 +4,62 @@ const Post = require('../models/Post.js');
 
 router
 
-    .get('/', (req, res) => {
-        res.send('We are on posts page');
-    })
-
-    .post('/', (req, res) => {
+    //creates and submits our post
+    .post('/', async (req, res) => {
+        const post = new Post({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        });
         try {
-            const post = req.body;
-            res.json(post);
+            const savePost = await post.save();
+            res.json(savePost);
         } catch (error) {
-            console.error(error.message)
+            res.json(error.message)
         }
     })
+
+    // gets all the post from our database
+    .get('/', async (req, res) => {
+        try {
+            const posts = await Post.find();
+            res.json(posts);
+        } catch (error) {
+            res.json(error.message);
+        }
+    })
+
+    //get specific post
+    .get('/:postId', async (req, res) => {
+        try {
+            const postId = req.params.postId;
+            const post = await Post.findById(postId);
+            res.json(post);
+        } catch (error) {
+            res.json(error.message)
+        }
+    })
+
+    //Update a post
+    .patch('/:postId', async (req, res) => {
+        try {
+            const updatePost = await Post.updateOne({_id : req.params.postId}, { $set: {title: req.body.title, content: req.body.content, author: req.body.author}});
+            res.json(updatePost);
+        } catch (error) {
+            res.json(error.message);
+        }
+    })
+
+    //delete a post by id
+    .delete('/:postId', async (req, res) => {
+        try {
+            const deletePost = await Post.remove({_id: req.params.postId});
+            res.json(deletePost);
+        } catch (error) {
+            res.json(error.message);
+        }
+    })
+
+
 
 module.exports = router;
